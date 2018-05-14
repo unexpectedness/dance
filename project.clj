a013f69db131b8e262555a27d21627142d7722d6
unexpectedness <simplertodescribethantogenerate@proton.me>
2018-05-14 14:37:04 +0200

    Add project URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dance "0.1.1"
   :description "Advanced tree walking in Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/unexpectedness/dance"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
